8df539491df8c2bd0c7f02af1a73dce2809b86bc
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-09-29 19:48:21 -0300

    Requisito 1 realizado

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,76 @@
 import React from 'react';
+import propTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+import { getEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    isBtnDisabled: true,
+  };
+
+  validateBtn = () => {
+    const { email, password } = this.state;
+    const minPass = 6;
+    const btnValidate = email.includes('@')
+    && email.includes('.com')
+    && password.length >= minPass;
+    this.setState({
+      isBtnDisabled: !(btnValidate),
+    });
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    }, this.validateBtn);
+  };
+
+  handleClick = () => {
+    const { dispatch, history } = this.props;
+    const { email } = this.state;
+    dispatch(getEmail(email));
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { isBtnDisabled } = this.state;
+
+    return (
+      <div>
+        Login
+        <input
+          type="text"
+          name="email"
+          data-testid="email-input"
+          placeholder="Email"
+          onChange={ this.handleChange }
+        />
+        <input
+          type="password"
+          name="password"
+          data-testid="password-input"
+          placeholder="Senha"
+          onChange={ this.handleChange }
+        />
+        <button
+          type="button"
+          disabled={ isBtnDisabled }
+          onClick={ this.handleClick }
+        >
+          Entrar
+
+        </button>
+      </div>
+    );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  history: propTypes.shape().isRequired,
+  dispatch: propTypes.func.isRequired,
+};
+
+export default connect()(Login);
--- src/redux/reducers/user.js
+++ src/redux/reducers/user.js
@@ -1,1 +1,20 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+import { GET_EMAIL } from '../actions';
+
+const INITAL_STATE = {
+  email: '',
+};
+
+const user = (state = INITAL_STATE, action) => {
+  switch (action.type) {
+  case GET_EMAIL:
+    return ({
+      ...state,
+      email: action.email,
+    });
+  default:
+    return state;
+  }
+};
+
+export default user;
