9d488a4788d71f49c42e4d9d72da5437e037fc44
Bernardo Lavinas <bernardorclavinas@gmail.com>
2022-09-29 19:57:07 -0300

    lint corrigido

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,7 @@
 // configure aqui sua store
 import { legacy_createStore as createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
